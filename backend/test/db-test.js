4a6ebec10f24b987b0ab99eb3acb721c3ec75e50
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-27 12:24:56 +0300

    Implement basic testing for db module

--- backend/test/db-test.js
+++ backend/test/db-test.js
@@ -1,7 +1,22 @@
 const expect = require("chai").expect;
+const db = require("../db");
+const mongoose = require("mongoose");
 
-describe("db tests", () => {
+describe("db tests", function() {
   it("should pass this canary test", () => {
     expect(true).to.be.true;
   });
+
+  it("connect should set connections given valid database name",
+    (done) => {
+      db.connectDB()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        expect(err).to.be.null;
+        done();
+      });
+    }
+  )
 });
