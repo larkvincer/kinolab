955182b2c3b665d2f2642649c423ead6e5d19a2f
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-27 20:57:26 +0300

    Add some css to header.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
 import { Link } from 'react-router-dom';
-
+import "../css/Header.css";
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <Link to="/">
-          <h1>Kinolab</h1>
+      <header className="Header">
+        <Link className="Header__logo" to="/">
+          <h1 className="margin_zero">Kinolab</h1>
         </Link>
         <Link to="/movie/add">
           <button>Add movie</button>
