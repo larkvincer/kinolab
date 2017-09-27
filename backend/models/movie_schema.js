9cd33697f9a9c9491e063a7cb9c959f76bf75845
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-27 12:25:33 +0300

    Create movie schema for mongodb

---
+++ backend/models/movie_schema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title:  {
+    type:String,
+    required: true,
+    minlength: 1
+  },
+  release_year: {
+    type: Number,
+    min: 1900,
+    max: 3000,
+    required: [true, "release year required"]
+  },
+  format: {
+    type: String,
+    enum: ["VHS", "DVD", "Blu-Ray"],
+    required: true
+  },
+  actors: [{
+    type: String
+  }]
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
