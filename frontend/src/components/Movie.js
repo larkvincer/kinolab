e64a2d12007003dd4d6e0e4623c82d02cbbc9738
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-28 00:18:50 +0300

    Implement delete functionality on front end.

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -1,25 +1,46 @@
 import React, {Component} from "react";
+import { Redirect } from 'react-router-dom';
 
 
 export default class Movie extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      redirect: false
+    };
   }
 
   componentDidMount() {
     fetch(`http://localhost:8080/movie/id/${this.props.match.params.id}`)
     .then(response => response.json())
-    .then(data => {this.setState({movie: data[0]})})
-    .catch(err => {this.setState({movie: "ERROR"})});
+    .then(data => {this.setState({...this.state, movie: data[0]})})
+    .catch(err => {this.setState({...this.state, movie: "ERROR"})});
     console.log(this.props.match.params.id);
   }
 
+  handleDelete() {
+    console.log(this.state.movie.id);
+    fetch(`http://localhost:8080/movie/${this.state.movie._id}`,
+    {method: "delete"})
+    .then((response) => response.json())
+    .then(data => {
+      alert("Movie successfuly deleted");
+      this.setState({...this.state, redirect: true});
+    })
+    .catch(error => {
+      alert("Service unavailable!!");
+    });
+  }
+
   render() {
+    if(this.state.redirect) {
+      return <Redirect to="/"/>
+    }
     const movie = this.state.movie;
     if(movie) {
       return (
         <article>
+          <button onClick={() => {this.handleDelete()}}>Delete</button>
           <img src="http://via.placeholder.com/300x500" alt={movie.title} />
           <h2>{movie.title}</h2>
           <div><strong>Realease year:</strong> {movie.release_year}</div>
