0b46d2c1c28664f082b63672f4f3601cd2a11a80
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-28 03:30:06 +0300

    Slightly refactored app.js.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const movieRouter = require("./routes/movie_route.js");
 const db = require("./db");
+
 const port = process.env.port || 8080;
-const cors = require("cors");
 
 app.use(cors());
 
@@ -17,21 +19,6 @@ db.connectDB().then(() => {
 }).catch(err => {
   console.log(err.message);
 });
-
-const Movie = require("./models/movie_schema");
-const movie = new Movie({
-  title: "som title",
-  release_year: 2000,
-  format: "DVD",
-  actors: ["Some actor"]
-});
-
-// movie.save((err) => {
-//   if (err)
-//     console.log(err);
-//   else
-//     console.log("successfuly saved");
-// });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
