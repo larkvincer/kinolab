0a3f748beeb548851500d9dc417ae39da89b15a8
Pavlo Kuziv <larkvincer@gmail.com>
2017-09-28 03:32:44 +0300

    Clean up movie_route.js.

--- backend/routes/movie_route.js
+++ backend/routes/movie_route.js
@@ -3,11 +3,8 @@ const express = require("express"),
   movieController = require("../controllers/movie_controller");
 
 movieRouter.get("/", movieController.listMovies);
-
 movieRouter.get("/id/:id", movieController.getMovieById);
-
 movieRouter.get("/title/:title", movieController.getMovieByTitle);
-
 movieRouter.get("/actor/:actor", movieController.getMovieByActor);
 
 movieRouter.post("/", movieController.addMovie);
